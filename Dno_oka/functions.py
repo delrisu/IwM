9af7c10cbcfccc2a5f93945cffa1eee5e6bee4ad
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-05-04 19:54:27 +0200

    Add files via upload

--- Dno_oka/functions.py
+++ Dno_oka/functions.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 import os
 import random
 import numpy as np
- 
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+
+from skimage.data import camera
+from skimage.filters import frangi, hessian
 from os import listdir
 from tqdm import tqdm 
 from skimage.transform import resize
@@ -124,4 +128,112 @@ def predict(image):
     image = conc(image)
     image = cv2.resize(image,(image_shape[1],image_shape[0]))
     return image
+
+
+def mask(image):
+    image = image[:,:,2]
+    _,mask = cv2.threshold(image,10,255,cv2.THRESH_BINARY)
+    return mask
+
+def simple(image):
+    kernel_ero = np.ones((3,3),np.uint8)
+    kernel_open = np.ones((11,11),np.uint8)
+    kernel_dil = np.ones((6,6),np.uint8)
+    
+    maskO = mask(image)
+    
+    image = image[:,:,1]
+    
+    image = cv2.GaussianBlur(image,(3,3),0)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
+    image = clahe.apply(image)
+    image = cv2.adaptiveThreshold(image,255,cv2.THRESH_BINARY,\
+                cv2.THRESH_BINARY,121,3)
+    image = cv2.dilate(image,kernel_dil,iterations = 1)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel_open)
+    image = cv2.erode(image,kernel_ero,iterations = 1)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel_open)
+
+    image = frangi(image)
+
+    image = image * maskO
+    image = cv2.normalize(image, None, 0, 1, cv2.NORM_MINMAX)
+    image[image>=0.9] = 1
+    image[image<0.1] = 0
+    return image
+
+def use_mask(image, mask):
+    image = image*1
+    for idx,x in enumerate(mask):
+        for idy,y in enumerate(x):
+            if(y > 0):
+                image[idx][idy][0] = 255
+                image[idx][idy][1] = 255
+                image[idx][idy][2] = 255
+                            
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def choose_file(window_title):
+    root = Tk()
+    filename = askopenfilename(initialdir = "./data/",title = window_title,filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+    root.destroy()
+    return filename
+    
+def statistics(reference_mask, mask, algorithm_name):
+    TN = 0
+    TP = 0
+    FN = 0
+    FP = 0
+    for idx,x in enumerate(reference_mask):
+        for idy,y in enumerate(x):
+            if (reference_mask[idx][idy]==0 and mask[idx][idy]==0):
+                TN+=1
+            if (reference_mask[idx][idy]>0 and mask[idx][idy]>0):
+                TP+=1
+            if (reference_mask[idx][idy]==0 and mask[idx][idy]>0):
+                FP+=1
+            if (reference_mask[idx][idy]>0 and mask[idx][idy]==0):
+                FN+=1
+    acc = (TP+TN)/(TN+TP+FN+FP)
+    spe = TN/(FP+TN)
+    sen = TP/(FN+TP)
+    return algorithm_name+"\nTrafność: "+str(acc)+"%\nCzułość: "+str(sen)+"%\nSwoistość: "+str(spe)+"%"        
+
+def start():
+    filename = choose_file("Wybierz zdjęcie dna oka")
+    image = cv2.imread(filename)
+    mask = cv2.imread(os.curdir+"/mask/"+filename.split("/")[-1])[:,:,2]
+    print("Obliczanie maski za pomocą prostego algorytmu...")
+    image_3 = simple(image)
+    print("Obliczanie maski za pomocą sieci neuronowych...")
+    image_5 = predict(image)
+    print("Nakładanie maski eksperckiej na obraz...")
+    masked_image = use_mask(image,mask)
+    print("Nakładanie maski prostego algorytmu na obraz...")
+    masked_image_simple = use_mask(image,image_3)
+    print("Nakładanie maski sieci neuronowej na obraz...")
+    masked_image_nn = use_mask(image,image_5)
+    fig = plt.figure(figsize=(20,20))
+    plt.subplot(2,2,1)
+    plt.title('Obraz dna oka')
+    plt.axis('off')
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.subplot(2,2,2)
+    plt.title('Maska ekspercka')
+    plt.axis('off')
+    plt.imshow(masked_image,cmap='gray')
+    plt.subplot(2,2,3)
+    plt.title("Proste filtrowania")
+    plt.axis('off')
+    plt.imshow(masked_image_simple,cmap='gray')
+    plt.subplot(2,2,4)
+    plt.title("Sieć neuronowa")
+    plt.axis('off')
+    plt.imshow(masked_image_nn,cmap='gray')
+    plt.show()
+    print("Obliczanie statystyk prostego algorytmu...")
+    print(statistics(mask,image_3,"Prosty algorytm"))
+    print("Obliczanie statystyk sieci neuronowej...")
+    print(statistics(mask,image_5,"Sieć neuronowa"))
+    
     
